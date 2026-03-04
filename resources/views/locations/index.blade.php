ebe5cbf72b1698bbc7effe3d930ab2b08cd0b44c
Faruk Hossain <faruktt143@gmail.com>
2026-03-04 10:52:29 +0600

    update

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -51,7 +51,10 @@
     </div>
  
     
-  
+    <div id="resultBox" class="alert alert-info mt-4 d-none">
+        <h5>আপনি যা নির্বাচন করেছেন:</h5>
+        <p id="resultText" class="mb-0"></p>
+    </div>
 </div>
 
 <script>
