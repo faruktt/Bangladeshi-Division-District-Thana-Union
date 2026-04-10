3bccb69e4992c69f4bcdf5a801a0a9c6d879b367
Faruk Hossain <faruktt143@gmail.com>
2026-04-10 17:41:27 +0600

    update

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -49,9 +49,7 @@
             <button id="showResult" class="btn btn-primary" disabled>Show Selection</button>
         </div>
     </div>
- 
-    
-       -->
+
     <div id="resultBox" class="alert alert-info mt-4 d-none">
         <h5>আপনি যা নির্বাচন করেছেন:</h5>
         <p id="resultText" class="mb-0"></p>
